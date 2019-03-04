349b3cbd8e054af1c04d3154e7a55c4da806f570
jshin029 <jshin029@ucr.edu>
2019-03-03 22:58:41 -0800

    finished sponsorship

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-function revealcontent() {
+    $(document).ready(function() {
+                
+                var documentEl = $(document),
+                    fadeElem = $('.row');
+                
+                
+                documentEl.on('scroll', function() {
+                    var currScrollPos = documentEl.scrollTop();
+                    
+                    fadeElem.each(function() {
+                        var $this = $(this),
+                            elemOffsetTop = $this.offset().top;
+                        if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+                    }); 
+                });
+                
+            });
+        
+
+
+function reveal() {
 	var coll = document.getElementsByClassName("collapsible");
-var i;
+	var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+	coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
@@ -15,3 +35,5 @@ for (i = 0; i < coll.length; i++) {
 }
 	
 }
+
+
