9b690c05cf4ac7ca9e21a36ea8306cf19130691c
jshin029 <jshin029@ucr.edu>
2019-03-31 14:54:39 -0700

    added ppt

--- js/main.js
+++ js/main.js
@@ -71,29 +71,56 @@ $(function() {
 	//~ startSlider();
 });
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides2 = document.getElementsByClassName("slides2");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides2.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides2.length}
-  for (i = 0; i < slides2.length; i++) {
-      slides2[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides2[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-} 
+'use strict';
+$(function() {
+	var width = $( document ).width()/2.256;
+	var animationSpeed = 450;
+	var pause = 3000;
+	var currentSlide = 1;
+	var interval;
+	
+	var $slider = $('#slider_1');
+	var $slideContainer = $slider.find('.slides_1');
+	var $slides = $slideContainer.find('.slide_1');
+	
+	$(".next_1").click(function() {
+		if (currentSlide < 5){
+		++currentSlide;
+		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+		});
+	}	
+		
+	});
+	
+	$(".prev_1").click(function() {
+		
+		if (currentSlide > 1){
+		--currentSlide;
+		$slideContainer.animate({'margin-left': '+='+width}, animationSpeed, function(){
+		});
+	}
+		
+	});
+	
+	
+	
+	function startSlider(){
+		interval = setInterval(function() {
+		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function(){
+			currentSlide++;
+			if(currentSlide === $slides.length-1) {
+				currentSlide = 1;
+				$slideContainer.css('margin-left', 0);
+			}
+		});
+	}, pause);
+	}
+	
+	//~ function stopSlider() {
+		//~ clearInterval(interval);
+	//~ }
+	
+	//~ $slider.on('mouseenter', stopSlider).on('mouseleave',startSlider);
+	
+	startSlider();
+});
